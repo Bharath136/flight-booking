21d4f75df705b966bca815ab34925d2953ec15d0
Bharath <bk3604073@gmail.com>
2023-11-25 12:52:50 +0530

    third-commit

--- server/src/db/connect.js
+++ server/src/db/connect.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-// Middleware
-const db = 'mongodb+srv://flight-booking:flight-booking@cluster0.hvf9qzd.mongodb.net/flight-booking?retryWrites=true&w=majority'
+
+
+const db = 'mongodb+srv://reeco:reeco@cluster0.6kqp7oh.mongodb.net/reeco?retryWrites=true&w=majority'
 
 // Connect to MongoDB using the connection string
 mongoose.connect(db, {
@@ -11,6 +12,3 @@ mongoose.connect(db, {
 }).catch((e) => {
   console.log(`No connection: ${e}`);
 });
-
-// mongodb://localhost:27017
-
